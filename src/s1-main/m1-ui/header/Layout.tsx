3d34a79377ca14381842b07053c00803ddae3218
Irina Novakovich <irinaredhood@gmail.com>
2023-02-19 23:22:25 +0300

    added header

--- src/s1-main/m1-ui/header/Layout.tsx
+++ src/s1-main/m1-ui/header/Layout.tsx
@@ -1,8 +1,10 @@
 import React, {FC, ReactNode} from 'react';
+import Header from "./Header";
 
 export const Layout: FC<PropsType> = ({children}) => {
     return (
         <div>
+            <Header/>
             <div>{children}</div>
         </div>
     );
--- src/s2-features/f2-password/PasswordRestore.tsx
+++ src/s2-features/f2-password/PasswordRestore.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PasswordRestore = () => {
     return (
         <div>
-
+            Password Restore
         </div>
     );
 };
